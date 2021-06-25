7b66f87180c0f4c6a9ed4b4385ed97a343106fc5
JoannaCoPo <jococopo@gmail.com>
2021-06-25 10:04:03 -0600

    Add models and controllers

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,45 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+    json_response(@todos)
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  # GET /todos/:id
+  def show
+    json_response(@todo)
+  end
+
+  # PUT /todos/:id
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  # callback method to find a todo by id. In the case where the record does not exist, ActiveRecord will throw
+  # an exception ActiveRecord::RecordNotFound. We’ll rescue from this exception and return a 404 message.
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    done { false }
-    todo { nil }
+    # name { Faker::StarWars.character }
+    name { Faker::Name.name  }
+    done false
+    todo_id nil
   end
 end
